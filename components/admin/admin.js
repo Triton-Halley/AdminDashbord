946bfa24d6b28024aa8fe401612ae80994f7d01b
Ali Geshani <Triton.halley@outlook.com>
2022-08-12 01:09:27 +0430

    add useSelector to update view , add switch statement to find current Page

--- components/admin/admin.js
+++ components/admin/admin.js
@@ -1,13 +1,42 @@
 import Menubar from "./menubar/menubar";
 import classes from "./admin.module.css";
+import { useSelector } from "react-redux";
+//pages
+import Overview from "./overview/overview";
+import Users from "./users/users";
+import Blog from "./blog//blog";
+import Product from "./product/product";
+import Setting from "./setting/setting";
 const AdminPage = () => {
+  const currentPage = useSelector((state) => state.dashboard.currentPage);
+  let page;
+  switch (currentPage) {
+    case "overview":
+      page = <Overview />;
+      break;
+    case "users":
+      page = <Users />;
+      break;
+    case "blog":
+      page = <Blog />;
+      break;
+    case "product":
+      page = <Product />;
+      break;
+    case "setting":
+      page = <Setting />;
+      break;
+    default:
+      page = <Overview />;
+      break;
+  }
   return (
     <>
       <div className={classes.container}>
         <div>
           <Menubar />
         </div>
-        <div className={classes.page}>page</div>
+        <div className={classes.page}>{page}</div>
       </div>
     </>
   );
